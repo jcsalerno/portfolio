c3a2aec61b9058a01f3d925a929b4d9ed0509f59
Julio Cesar <contato.jcsalerno@gmail.com>
2023-11-29 21:19:16 -0300

    Ajuste no JavaScript para mobile

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// create auto navbar scrolling
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll("header nav a");
+  const menubar = document.querySelector("#menu");
+  const navbar = document.querySelector(".navbar");
 
   navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
@@ -13,9 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
       if (targetSection) {
         targetSection.scrollIntoView({ behavior: "smooth" });
 
-        // Remova a classe 'active' de todos os links e adicione àquele que foi clicado
         navLinks.forEach((link) => link.classList.remove("active"));
         this.classList.add("active");
+
+        menubar.classList.remove("bx-x");
+        navbar.classList.remove("active");
       }
     });
   });
@@ -30,21 +33,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (top >= offset && top < offset + height) {
         navLinks.forEach((link) => link.classList.remove("active"));
-
-        // Adicione a classe 'active' ao link correspondente
         document
           .querySelector(`header nav a[href='#${id}']`)
           .classList.add("active");
       }
     });
   };
-  // Create a sticky nav bar
+
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 100);
   menubar.classList.remove("bx-x");
   navbar.classList.remove("active");
-
-  // Create a toogle changer
 });
 
 let menubar = document.querySelector("#menu");
